7939e31c96bcb67514df95ca4ccbac2027ba512c
zxj <zhuxiujia@qq.com>
2022-03-11 11:07:47 +0800

    add example

---
+++ example/src/main_set_frame_size.rs
@@ -0,0 +1,40 @@
+use std::future::Future;
+use std::io::Sink;
+use std::marker::PhantomData;
+use std::net::SocketAddr;
+use std::pin::Pin;
+use std::process::exit;
+use std::time::Duration;
+use dark_std::err;
+use fast_log::config::Config;
+use fast_log::filter::ModuleFilter;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use drpc::client::Client;
+use drpc::codec::{Codecs, JsonCodec};
+use drpc::server::{Handler, Server, Stub};
+use drpc::Result;
+use tokio::time::sleep;
+
+pub async fn handle(req: i32) -> drpc::Result<i32> {
+     Ok(req + 1)
+}
+
+#[tokio::main]
+async fn main() {
+    fast_log::init(Config::new().console());
+
+    drpc::set_frame_len(10 * 1024 * 1024);//10MB
+    tokio::spawn(async move {
+        sleep(Duration::from_secs(1)).await;
+        let c = Client::dial("127.0.0.1:10000").await.unwrap();
+        println!("dial success");
+        let resp: i32 = c.call("handle", 1).await.unwrap();
+        println!("resp=>>>>>>>>>>>>>> :{}", resp);
+        exit(0);
+    });
+    let mut s = Server::default();
+    s.register_fn("handle", handle);
+    s.serve("0.0.0.0:10000").await;
+    println!("Hello, world!");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -21,8 +21,18 @@ pub mod frame;
 pub mod balance;
 pub mod balance_manager;
 
+use std::sync::atomic::Ordering;
 pub use balance_manager::*;
 
 
 pub use dark_std::errors::Error;
 pub use dark_std::errors::Result;
+
+/// set_frame_len
+pub fn set_frame_len(size: u64) {
+    crate::frame::FRAME_MAX_LEN.store(size, Ordering::SeqCst);
+}
+
+pub fn get_frame_len() -> u64 {
+    crate::frame::FRAME_MAX_LEN.load(Ordering::SeqCst)
+}
