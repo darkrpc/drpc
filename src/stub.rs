2e2a5a2acbb9b309173f798f3849918f5e68711f
zxj <zhuxiujia@qq.com>
2022-03-15 16:26:52 +0800

    add call_frame for client

--- src/stub.rs
+++ src/stub.rs
@@ -4,6 +4,8 @@ use std::io::ErrorKind;
 use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, BufReader};
 
 use std::ops::Index;
+use std::pin::Pin;
+use std::sync::Arc;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::time::Duration;
 use dark_std::err;
@@ -35,10 +37,10 @@ impl ClientStub {
         }
     }
 
-    pub async fn call_frame<C: Codec, Arg: Serialize, Resp: DeserializeOwned, F, Transport>(&self, method: &str, arg: Arg, codec: &C, transport: Transport) -> Result<Resp>
+    pub async fn call_frame<C: Codec, Arg: Serialize, Resp: DeserializeOwned, F, Transport>(&self, method: &str, arg: Arg, codec: &C, mut transport: Transport) -> Result<Resp>
         where
             F: Future<Output=Frame>,
-            Transport: Fn(Frame) -> F {
+            Transport: FnOnce(Frame) -> F {
         let mut arg_data = method.to_string().into_bytes();
         arg_data.push('\n' as u8);
         arg_data.extend(codec.encode(arg)?);
@@ -70,54 +72,49 @@ impl ClientStub {
     }
 
     pub async fn call<C: Codec, Arg: Serialize, Resp: DeserializeOwned, S>(&self, method: &str, arg: Arg, codec: &C, mut stream: S) -> Result<Resp> where S: AsyncRead + AsyncWrite + Unpin {
-        let mut arg_data = method.to_string().into_bytes();
-        arg_data.push('\n' as u8);
-        arg_data.extend(codec.encode(arg)?);
-        let mut req_buf = Frame::new();
-        req_buf.write_all(&arg_data).await?;
-        let id = {
-            let mut id = self.tag.load(Ordering::SeqCst);
-            if id == u64::MAX {
-                id = 0;
-            } else {
-                id += 1;
+        self.call_frame(method, arg, codec, move |req_buf: Frame| async move {
+            let id = req_buf.id;
+            let data = req_buf.finish(id, true);
+            if let Err(e) = stream.write_all(&data).await {
+                return Frame {
+                    id,
+                    ok: 0,
+                    data: e.to_string().into_bytes(),
+                };
             }
-            self.tag.store(id, Ordering::SeqCst);
-            id
-        };
-        debug!("request id = {}", id);
-        let data = req_buf.finish(id, true);
-        stream.write_all(&data).await?;
-        let mut time_start = None;
-        if self.timeout.is_some() {
-            time_start = Some(std::time::Instant::now());
-        }
-        // read the response
-        loop {
-            // deserialize the rsp
-            let rsp_frame = Frame::decode_from(&mut stream).await.map_err(|e| Error::from(e))?;
-            // discard the rsp that is is not belong to us
-            if rsp_frame.id == id {
-                debug!("get response id = {}", id);
-                if rsp_frame.ok == 0 {
-                    let rsp_data = rsp_frame.get_payload();
-                    let resp: String = unsafe { String::from_utf8_unchecked(rsp_data.to_vec()) };
-                    return Err(Error { inner: resp });
-                } else {
-                    let rsp_data = rsp_frame.get_payload();
-                    let resp: Resp = codec.decode(rsp_data)?;
-                    return Ok(resp);
+            let mut time_start = None;
+            if self.timeout.is_some() {
+                time_start = Some(std::time::Instant::now());
+            }
+            loop {
+                // deserialize the rsp
+                let rsp_frame = Frame::decode_from(&mut stream).await.map_err(|e| Error::from(e));
+                if rsp_frame.is_err() {
+                    return Frame {
+                        id,
+                        ok: 0,
+                        data: rsp_frame.err().unwrap().to_string().into_bytes(),
+                    };
                 }
-            } else {
+                let rsp_frame = rsp_frame.unwrap();
+                // discard the rsp that is is not belong to us
+                if rsp_frame.id == id {
+                    debug!("get response id = {}", id);
+                    return rsp_frame;
+                }
                 if let Some(timeout) = self.timeout {
                     if let Some(time_start) = &time_start {
                         if time_start.elapsed() > timeout {
-                            return Err(err!("rpc call timeout!"));
+                            return Frame {
+                                id,
+                                ok: 0,
+                                data: "rpc call timeout!".to_string().into_bytes(),
+                            };
                         }
                     }
                 }
             }
-        }
+        }).await
     }
 }
 
