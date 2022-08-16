0296a1918f0d22afa0350785ee07a88907feda5e
zxj <zhuxiujia@qq.com>
2022-08-16 09:56:27 +0800

    edit bench

--- benches/latency.rs
+++ benches/latency.rs
@@ -31,23 +31,17 @@ fn latency(bencher: &mut Bencher) {
         s.serve(("127.0.0.1", 10000)).await;
         println!("rpc served");
     });
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build().unwrap();
-    let c = rt.block_on(async {
-        sleep(Duration::from_secs(1));
-        let mut c = Client::<BinCodec>::dial("127.0.0.1:10000").await.unwrap();
-        println!("dial success");
-        let resp: i32 = c.call("handle", 1).await.unwrap();
-        println!("resp=>>>>>>>>>>>>>> :{}", resp);
-        c
-    });
     let (s, r) = channel();
     std::thread::spawn(move ||{
         let rt = tokio::runtime::Builder::new_current_thread()
             .enable_all()
             .build().unwrap();
         rt.block_on(async {
+            sleep(Duration::from_secs(1));
+            let mut c = Client::<BinCodec>::dial("127.0.0.1:10000").await.unwrap();
+            println!("dial success");
+            let resp: i32 = c.call("handle", 1).await.unwrap();
+            println!("resp=>>>>>>>>>>>>>> :{}", resp);
             loop {
                 match r.recv() {
                     Ok(v) => {
@@ -58,7 +52,8 @@ fn latency(bencher: &mut Bencher) {
             }
         });
     });
-    sleep(Duration::from_secs(1));
+    s.send(1).expect("panic send");
+    sleep(Duration::from_secs(2));
     bencher.iter(|| {
         s.send(1).expect("panic send");
     });
