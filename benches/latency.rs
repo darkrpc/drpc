5cb3e69d2e4a0aa30322271d953f9ebca920876b
zxj <zhuxiujia@qq.com>
2022-08-16 09:51:49 +0800

    edit bench

--- benches/latency.rs
+++ benches/latency.rs
@@ -7,9 +7,10 @@ extern crate test;
 #[macro_use]
 extern crate tokio;
 
+use std::sync::mpsc::channel;
+use std::thread::sleep;
 use std::time::Duration;
 use test::Bencher;
-use tokio::time::sleep;
 use drpc::client::Client;
 use drpc::codec::BinCodec;
 use drpc::server::Server;
@@ -34,16 +35,31 @@ fn latency(bencher: &mut Bencher) {
         .enable_all()
         .build().unwrap();
     let c = rt.block_on(async {
-        sleep(Duration::from_secs(1)).await;
+        sleep(Duration::from_secs(1));
         let mut c = Client::<BinCodec>::dial("127.0.0.1:10000").await.unwrap();
         println!("dial success");
         let resp: i32 = c.call("handle", 1).await.unwrap();
         println!("resp=>>>>>>>>>>>>>> :{}", resp);
         c
     });
-    bencher.iter(|| {
+    let (s, r) = channel();
+    std::thread::spawn(move ||{
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build().unwrap();
         rt.block_on(async {
-            let resp: i32 = c.call("handle", 1).await.unwrap();
+            loop {
+                match r.recv() {
+                    Ok(v) => {
+                        let resp: i32 = c.call("handle", v).await.unwrap();
+                    }
+                    Err(e) => {}
+                }
+            }
         });
+    });
+    sleep(Duration::from_secs(1));
+    bencher.iter(|| {
+        s.send(1).expect("panic send");
     });
 }
